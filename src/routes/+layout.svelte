7b307de65114c9ab62d92838a99252592bdc86b9
Antonin Jilek <antonin.jilek@webatelier.cz>
2023-01-28 16:15:52 +0100

    dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,16 @@
     $: homepage = !$page.params.type
 </script>
 
+
 {#if homepage}
-<div class="mx-6 xl:mx-0 mb-4">
-    <div class="max-w-7xl mx-auto pt-10">
+<div class="bg-slate-100 dark:bg-pbw-dark">
+    <div class="max-w-7xl mx-auto py-20">
         <h1 class="text-5xl uppercase font-bold text-pbw-red"><a href="/{$page.params.entry}">Prague Blockchain Week 2023</a></h1>
         <div class="text-4xl text-pbw-yellow">June 2-11, 2023</div>
     </div>
 </div>
 {/if}
 
-<div class="mb-24">
+<div class="pb-24 bg-slate-100 dark:bg-pbw-dark">
     <slot />
 </div>
