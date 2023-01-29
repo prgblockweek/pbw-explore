a05b73deb5c8fafb20e8351239b10366086441c8
tree <tree@hidden.cz>
2023-01-29 09:21:45 +0100

    Add event types to days

--- src/lib/components/CalendarList.svelte
+++ src/lib/components/CalendarList.svelte
@@ -1,6 +1,7 @@
 <script>
     import { format } from 'date-fns';
     import ItemLogo from '$lib/components/ItemLogo.svelte';
+    import EventTypeBadge from '$lib/components/EventTypeBadge.svelte';
 
     export let segments;
     export let date;
@@ -11,23 +12,41 @@
 
 <div class="text-2xl">
     {#each segments as segment}
-        <div class="md:flex flex-wrap mb-2 gap-3 items-center text-gray-800">
-            <div class="text-xl md:w-32 md:text-right">{format(new Date(segment.startTime), "HH:mm")} - {format(new Date(segment.endTime), "HH:mm")}</div>
+        <div class="md:flex flex-wrap mb-2 gap-3 items-center text-gray-800 border border-gray-300 rounded-lg md:border-none px-3 py-2 md:px-0 md:py-0">
+            <div class="flex gap-2">
+                <div class="text-xl md:w-32 md:text-right">{format(new Date(segment.startTime), "HH:mm")} - {format(new Date(segment.endTime), "HH:mm")}</div>
+                {#if !event}
+                    <div class="gap-1 items-center md:ml-2 inline-flex md:hidden text-xs">
+                        {#each (segment.event || event).types as type}
+                            <EventTypeBadge {type} />
+                        {/each}
+                    </div>
+                {/if}
+            </div>
             <div class="md:flex gap-2 items-center mb-6 md:mb-0">
                 <div class="flex gap-2 items-center my-2 md:my-0">
                     <a href="/{entry}/event/{(segment.event || event).id}">
                         <ItemLogo item={segment.event || event} width={event ? 'w-8' : 'w-10'}/>
                     </a>
-                    {#if !event}
-                        <a href="/{entry}/event/{(segment.event || event).id}" class="text-pbw-red hover:underline">{(segment.event || event)[event && event.shortname ? 'shortname' : 'name']}</a>
-                    {:else}
-                        <span class="">{event.shortname || event.name}</span>
-                    {/if}
-                    {#if segment.title}
-                        - {segment.title}
-                    {/if}
+                    <div>
+                        {#if !event}
+                            <a href="/{entry}/event/{(segment.event || event).id}" class="text-pbw-red hover:underline">{(segment.event || event)[event && event.shortname ? 'shortname' : 'name']}</a>
+                        {:else}
+                            <span class="">{event.shortname || event.name}</span>
+                        {/if}
+                        {#if segment.title}
+                            - {segment.title}
+                        {/if}
+                    </div>
                 </div>
                 <div class="flex flex-wrap gap-2">
+                    {#if !event}
+                        <div class="gap-1 items-center md:ml-2 hidden md:inline-flex text-xs">
+                            {#each (segment.event || event).types as type}
+                                <EventTypeBadge {type} />
+                            {/each}
+                        </div>
+                    {/if}
                     <div class="text-xl">
                         {#if segment.venues || event?.venues}
                             📍 {@html ((segment.venues || event.venues).map(vId => {
