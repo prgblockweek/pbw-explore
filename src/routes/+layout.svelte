c1e019f0655afa703cec7070574c81e800fd5cf9
tree <tree@hidden.cz>
2023-01-30 04:02:04 +0100

    Link main logo to pbw hp

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 {#if homepage}
     <div class="px-6 xl:px-0 dark:bg-pbw-dark">
         <div class="max-w-7xl mx-auto pt-10 pb-6">
-            <h1 class="text-5xl uppercase font-bold text-pbw-red"><a href="/{$page.params.entry}">Prague Blockchain Week 2023</a></h1>
+            <h1 class="text-5xl uppercase font-bold text-pbw-red"><a href="https://prgblockweek.com">Prague Blockchain Week 2023</a></h1>
             <div class="text-4xl text-pbw-yellow">June 2-11, 2023</div>
         </div>
     </div>
